a703bad6d39258fa18d780816779b046c6954a31
Baccast <jeremybl2004@gmail.com>
2023-09-28 21:34:20 -0400

    made changes

--- Thermoresistor.py
+++ Thermoresistor.py
@@ -3,15 +3,16 @@ import ADC0832
 import time
 import math
 import tkinter as tk
-import threading  # Import the threading module
+import threading
 
 # Constants for the thermistor characteristics
 R0 = 10000  # Resistance at a known temperature (in ohms)
 T0 = 25     # Known temperature in Celsius (adjust as needed)
 B = 3950    # Beta coefficient of the thermistor (adjust as needed)
 
-# Global variable to store temperature in Celsius
+# Global variables
 temperature_Celsius = 0.0
+temperature_threshold = 0.0
 
 def init():
     ADC0832.setup()
@@ -22,22 +23,33 @@ def temperature_from_resistance(Rt):
     temperature_C = 1.0 / inv_T - 273.15
     return temperature_C
 
-def update_temperature():
-    global temperature_Celsius
+def update_temperature_and_threshold():
+    global temperature_Celsius, temperature_threshold
 
     while True:
-        res = ADC0832.getADC(0)
-        Vr = 3.3 * float(res) / 255
-        Rt = 10000 * Vr / (3.3 - Vr)
+        # Read temperature and potentiometer values from ADC channels
+        res_temp = ADC0832.getADC(0)  # Temperature sensor connected to channel 0
+        res_pot = ADC0832.getADC(1)   # Potentiometer connected to channel 1
+
+        Vr_temp = 3.3 * float(res_temp) / 255
+        Vr_pot = 3.3 * float(res_pot) / 255
+
+        Rt_temp = 10000 * Vr_temp / (3.3 - Vr_temp)
         
-        temperature_C = temperature_from_resistance(Rt)
+        # Calculate temperature in Celsius
+        temperature_C = temperature_from_resistance(Rt_temp)
         temperature_F = (temperature_C * 9/5) + 32  # Convert to Fahrenheit
 
         # Update the global temperature variable
         temperature_Celsius = temperature_C
 
-        # Update the GUI label text
+        # Calculate temperature threshold from the potentiometer value
+        # Assuming the potentiometer range is from -50C to 50C
+        temperature_threshold = (Vr_pot * 100) - 50
+
+        # Update the GUI labels
         temperature_label.config(text=f'Temperature (Celsius): {temperature_C:.2f}°C\nTemperature (Fahrenheit): {temperature_F:.2f}°F')
+        threshold_label.config(text=f'Temperature Threshold: {temperature_threshold:.2f}°C')
 
         time.sleep(0.2)
 
@@ -48,18 +60,21 @@ def main():
     root = tk.Tk()
     root.title("Thermistor Temperature Monitor")
 
-    # Create a label to display temperature
-    global temperature_label
+    # Create labels to display temperature and threshold
+    global temperature_label, threshold_label
     temperature_label = tk.Label(root, text="", font=("Helvetica", 16))
-    temperature_label.pack(padx=20, pady=20)
+    temperature_label.pack(padx=20, pady=10)
+
+    threshold_label = tk.Label(root, text="", font=("Helvetica", 16))
+    threshold_label.pack(padx=20, pady=10)
 
     # Create a button to exit the application
     exit_button = tk.Button(root, text="Exit", command=root.quit)
     exit_button.pack()
 
     # Start the temperature update thread
-    update_thread = threading.Thread(target=update_temperature)
-    update_thread.daemon = True  # Allow the thread to exit when the main program exits
+    update_thread = threading.Thread(target=update_temperature_and_threshold)
+    update_thread.daemon = True
     update_thread.start()
 
     root.mainloop()
