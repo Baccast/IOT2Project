9092127d2189e9207f700caded17f562ad3db7cb
Baccast <jeremybl2004@gmail.com>
2023-09-28 21:39:50 -0400

    made changes

--- Thermoresistor.py
+++ Thermoresistor.py
@@ -18,10 +18,14 @@ def init():
     ADC0832.setup()
 
 def temperature_from_resistance(Rt):
-    # Calculate temperature in Celsius using the Steinhart-Hart equation
-    inv_T = 1.0 / (T0 + 273.15) + (1.0 / B) * math.log(Rt / R0)
-    temperature_C = 1.0 / inv_T - 273.15
-    return temperature_C
+    try:
+        # Calculate temperature in Celsius using the Steinhart-Hart equation
+        inv_T = 1.0 / (T0 + 273.15) + (1.0 / B) * math.log(Rt / R0)
+        temperature_C = 1.0 / inv_T - 273.15
+        return temperature_C
+    except ValueError:
+        # Handle the case where math.log() receives an invalid argument
+        return None
 
 def update_temperature_and_threshold():
     global temperature_Celsius, temperature_threshold
@@ -38,18 +42,20 @@ def update_temperature_and_threshold():
         
         # Calculate temperature in Celsius
         temperature_C = temperature_from_resistance(Rt_temp)
-        temperature_F = (temperature_C * 9/5) + 32  # Convert to Fahrenheit
+        
+        if temperature_C is not None:
+            temperature_F = (temperature_C * 9/5) + 32  # Convert to Fahrenheit
 
-        # Update the global temperature variable
-        temperature_Celsius = temperature_C
+            # Update the global temperature variable
+            temperature_Celsius = temperature_C
 
-        # Calculate temperature threshold from the potentiometer value
-        # Assuming the potentiometer range is from -50C to 50C
-        temperature_threshold = (Vr_pot * 100) - 50
+            # Calculate temperature threshold from the potentiometer value
+            # Assuming the potentiometer range is from -50C to 50C
+            temperature_threshold = (Vr_pot * 100) - 50
 
-        # Update the GUI labels
-        temperature_label.config(text=f'Temperature (Celsius): {temperature_C:.2f}°C\nTemperature (Fahrenheit): {temperature_F:.2f}°F')
-        threshold_label.config(text=f'Temperature Threshold: {temperature_threshold:.2f}°C')
+            # Update the GUI labels
+            temperature_label.config(text=f'Temperature (Celsius): {temperature_C:.2f}°C\nTemperature (Fahrenheit): {temperature_F:.2f}°F')
+            threshold_label.config(text=f'Temperature Threshold: {temperature_threshold:.2f}°C')
 
         time.sleep(0.2)
 
