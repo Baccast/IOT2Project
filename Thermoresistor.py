2eb1e1a5df4b58a3999d5175e68659ec2dc78c7c
Baccast <jeremybl2004@gmail.com>
2023-09-28 22:47:03 -0400

    made changes

--- Thermoresistor.py
+++ Thermoresistor.py
@@ -14,9 +14,12 @@ B = 3950    # Beta coefficient of the thermistor (adjust as needed)
 # Global variables
 temperature_Celsius = 0.0
 temperature_threshold = 0.0
+alarm_status = False  # Alarm status (True if on, False if off)
 
-# GPIO pin for the buzzer
+# GPIO pins
 BUZZER_PIN = 23
+RED_BUTTON_PIN = 24
+BLUE_BUTTON_PIN = 25
 
 # Define the potentiometer reading range
 POT_MIN = 0    # Minimum ADC value for the potentiometer
@@ -27,6 +30,8 @@ def init():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(BUZZER_PIN, GPIO.OUT)
+    GPIO.setup(RED_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Red button
+    GPIO.setup(BLUE_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Blue button
     GPIO.output(BUZZER_PIN, GPIO.LOW)  # Turn off the buzzer initially
 
 def map_value(value, from_min, from_max, to_min, to_max):
@@ -42,6 +47,21 @@ def temperature_from_resistance(Rt):
     except ValueError:
         # Handle the case where math.log() receives an invalid argument
         return None
+
+def update_alarm_status():
+    global alarm_status
+
+    while True:
+        if GPIO.input(RED_BUTTON_PIN) == GPIO.LOW:
+            # Red button pressed, turn off the alarm
+            alarm_status = False
+            print("Alarm OFF")
+        elif GPIO.input(BLUE_BUTTON_PIN) == GPIO.LOW:
+            # Blue button pressed, turn on the alarm
+            alarm_status = True
+            print("Alarm ON")
+
+        time.sleep(0.1)
 
 def update_temperature_and_threshold():
     global temperature_Celsius, temperature_threshold
@@ -66,7 +86,7 @@ def update_temperature_and_threshold():
             temperature_Celsius = temperature_C
 
             # Map potentiometer value to temperature threshold
-            temperature_threshold = map_value(res_pot, POT_MIN, POT_MAX, 50, -50)  # Inverted mapping
+            temperature_threshold = map_value(res_pot, POT_MIN, POT_MAX, -50, 50)
 
             # Ensure the threshold doesn't exceed the specified range
             temperature_threshold = max(-50, min(50, temperature_threshold))
@@ -74,9 +94,9 @@ def update_temperature_and_threshold():
             # Update the GUI labels
             temperature_label.config(text=f'Temperature (Celsius): {temperature_C:.2f}°C\nTemperature (Fahrenheit): {temperature_F:.2f}°F')
             threshold_label.config(text=f'Temperature Threshold: {temperature_threshold:.2f}°C')
-
-            # Check if temperature exceeds the threshold
-            if temperature_C > temperature_threshold:
+            
+            # Check if the alarm is on and temperature exceeds the threshold
+            if alarm_status and temperature_C > temperature_threshold:
                 # Turn on the buzzer
                 GPIO.output(BUZZER_PIN, GPIO.HIGH)
             else:
@@ -100,14 +120,15 @@ def main():
     threshold_label = tk.Label(root, text="", font=("Helvetica", 16))
     threshold_label.pack(padx=20, pady=10)
 
-    # Create a button to exit the application
-    exit_button = tk.Button(root, text="Exit", command=root.quit)
-    exit_button.pack()
-
     # Start the temperature update thread
     update_thread = threading.Thread(target=update_temperature_and_threshold)
     update_thread.daemon = True
     update_thread.start()
+
+    # Start the alarm status update thread
+    alarm_thread = threading.Thread(target=update_alarm_status)
+    alarm_thread.daemon = True
+    alarm_thread.start()
 
     root.mainloop()
 
